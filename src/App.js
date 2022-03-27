28a257a95e8c49b87045348267db5ff47db3d5e4
nnnabil <rhnabil.aiub@gmail.com>
2022-03-27 11:35:35 +0600

    react init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Adv Web Spring 21-22
         </p>
         <a
           className="App-link"
